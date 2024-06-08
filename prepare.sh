c3130f191b833f2acc0fc45a8e668b0aac3f20a1
as247 <as247@vui360.com>
2024-06-08 13:12:00 +0700

    Update prepare

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,10 @@ fi
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 # Navigate to the script's directory
 cd "$SCRIPT_DIR" || exit
-source ./.env
+# Load environment variables
+if [ -f .env ]; then
+  source ./.env
+fi
 architecture=$(uname -m)
 passwordLength=18
 if [ "$architecture" == "x86_64" ]; then
@@ -14,21 +17,7 @@ if [ "$architecture" == "x86_64" ]; then
 else
   MYSQL_IMAGE="mysql:8.0.37"
 fi
-#if MYSQL_PORT is not set then set it to 3306
-if [ -z "$MYSQL_PORT" ]; then
-  MYSQL_PORT=127.0.0.1:3306
-fi
-#if PHPMYADMIN_PORT is not set then set it to 8801
-if [ -z "$PHPMYADMIN_PORT" ]; then
-  PHPMYADMIN_PORT=8801
-fi
-
-
-
-chmod +x *.sh
 echo "MYSQL_IMAGE=$MYSQL_IMAGE" > .env
-echo "MYSQL_PORT=$MYSQL_PORT" >> .env
-echo "PHPMYADMIN_PORT=$PHPMYADMIN_PORT" >> .env
 
 if [ ! -f mysql.env ]; then
   echo "mysql.env not found, copying from mysql.env.example"
@@ -40,8 +29,6 @@ if [ ! -f mysql.env ]; then
   #read docker-compose.yml file for root host
   root_host=$(grep -oP '(?<=subnet: ).*' docker-compose.yml | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk -F. '{print $1"."$2"."$3".%"}')
   sed -i "s/MYSQL_ROOT_HOST=.*/MYSQL_ROOT_HOST=\"$root_host\"/" mysql.env
-
-
 fi
 
 mkdir -p mysql/data
@@ -62,6 +49,8 @@ fi
 
 chown -R 999:999 mysql/data
 chown -R 999:999 mysql/log
-echo "Mysql is ready. Run 'docker compose up -d' to start the mysql server"
+docker compose up -d
+echo "Mysql is ready"
+echo "Root password: $root_password"
 
 
